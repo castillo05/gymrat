0c856f8463a703c02bdc93c78775a44947b2b2f2
Jorge Castillo <jorge.castillo@planvital.cl>
2023-03-30 08:40:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mastergym
+
+Proyecto generado generado con Angular version 8.1.1.
+
+## Instalación
+
+Primero ubicarse en una capeta luego ejecutar
+`git clone https://gitlab.com/tom07/mastergymangular.git`
+
+Luego acceder a la ruta nueva `cd mastergymangular`
+hacer `npm install`
+y por ultimo ejecutar el `ng serve --o` 
+
+## 
+Saludos
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDYwqnlscZ7uMYwnLrmCIBXKypx6nrwWyo",
+    authDomain: "mueverde-test.firebaseapp.com",
+    databaseURL: "https://mueverde-test-default-rtdb.firebaseio.com",
+    projectId: "mueverde-test",
+    storageBucket: "mueverde-test.appspot.com",
+    messagingSenderId: "384464164485",
+    appId: "1:384464164485:web:a4cad9cbd315924d12fbec",
+    measurementId: "G-84FTF255Z6"
+  }
+};
+
+//Esto lo encontraran en clase Agregar AngularFire2 del proyecto final
